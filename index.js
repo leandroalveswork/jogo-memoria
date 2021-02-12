e2e8b6035b578fb5ef5d97ca3ea2105335ca4e5d
leandroalveswork <leandroprogramadoruxdesign@gmail.com>
2021-02-12 10:45:18 -0300

    clean code

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.get("/", (req, res) => {
-    res.send('funcionando!')
-})
-
 app.listen(process.env.PORT || 3000, () => {
     console.log('Joguinho rodando!')
 })
