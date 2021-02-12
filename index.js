b091158076be7d4c7002c496374e4e6d22e6c5ed
leandroalveswork <leandroprogramadoruxdesign@gmail.com>
2021-02-12 11:18:30 -0300

    add favicon

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 const app = express()
 
 app.use(express.static('public'))
+app.use(express.favicon('public/img/favicon.ico')); 
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Joguinho rodando!')
